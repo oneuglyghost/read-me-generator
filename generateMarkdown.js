18d006591c2db5e5e73551842b6ed720a1df07ac
Bernardo Alonso <bernardo4430@gmail.com>
2024-01-20 14:08:30 -0600

    made required functions

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,17 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License](https://img.shields.io/badge/License-${license}-brightgreen.svg)] (https://opensource.org/licenses/${license})`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license){
+    return `[License](https://opensource.org/licenses/${license})`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license){
+    return `## License
+    This project is licensed under the [${license}](https://opensource.org/licenses/${license}) license.`;
+  }
+  return "";
+}
+console.log("works")
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection =renderLicenseSection(data.license);
+
   return `# ${data.title}
 
 `;
